948283c97503773832e9cd07522558739a461ef6
ltkwan <letitia.kwan@gmail.com>
2021-11-13 22:27:54 -0500

    remove useEffect

--- src/components/ExportListButton.jsx
+++ src/components/ExportListButton.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import '../../src/index.css'
 import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 import { CSVLink } from "react-csv";
